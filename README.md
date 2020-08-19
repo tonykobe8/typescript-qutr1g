08fa22ca4faf161a088355a15d85c86a8142f22b
tonykobe8 <tonykobe8@gmail.com>
2020-08-19 22:31:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-qutr1g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-qutr1g)
---
+++ index.ts
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+function max_value(x,y){
+  return x > y;
+}
+console.log (max (4,7));
